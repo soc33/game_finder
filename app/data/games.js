677b0ecb74d14b5580d6bc7502169e02edfd59bd
soc33 <soc33@msn.com>
2019-02-26 23:27:15 -0500

    almost done!

---
+++ app/data/games.js
@@ -0,0 +1,54 @@
+var games = [
+    {
+        name: "Lords of Waterdeep",
+        photo: "./images/lords.jpg",
+        scores: [2, 1, 5, 2, 4, 4, 3, 5, 2, 3]
+    },
+    {
+        name: "Betrayal at House on the Hill",
+        photo: "./images/betrayal.jpeg",
+        scores: [5, 2, 1, 5, 3, 3, 4, 5, 3, 5]
+    },
+    {
+        name: "Monopoly",
+        photo: "./images/monopoly.jpg",
+        scores: [2, 1, 5, 2, 5, 1, 5, 5, 2, 4]
+    },
+    {
+        name: "Code Names",
+        photo: "./images/code.jpg",
+        scores: [1, 2, 1, 5, 5, 5, 1, 4, 4, 3]
+    },
+    {
+        name: "Forbidden Island",
+        photo: "./images/forbidden.jpg",
+        scores: [5, 1, 2, 5, 1, 1, 3, 3, 2, 3]
+    },
+    {
+        name: "Zombicide",
+        photo: "./images/zombie.jpg",
+        scores: [5, 5, 2, 5, 1, 1, 5, 5, 3, 5]
+    },
+    {
+        name: "King of Tokyo",
+        photo: "./images/king.jpg",
+        scores: [1, 4, 4, 1, 5, 3, 2, 4, 3, 3]
+    },
+    {
+        name: "Sushi Go",
+        photo: "./images/sushiGo.jpg",
+        scores: [1, 1, 3, 1, 5, 5, 2, 5, 5, 5]
+    },
+    {
+        name: "Exploding Kittens",
+        photo: "./images/exploding.png",
+        scores: [1, 4, 1, 1, 5, 5, 3, 2, 5, 4]
+    },
+    {
+        name: "Boss Monster",
+        photo: "./images/boss.jpg",
+        scores: [5, 5, 5, 2, 5, 4, 3, 2, 2, 3]
+    },
+];
+
+module.exports = games;
---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,75 @@
+var games = require('../data/games.js');
+var newGames = [];
+
+module.exports = function (app) {
+
+    app.get('/api/gameList', function (req, res) {
+        res.json(games);
+    });
+
+    app.post('/api/gameList', function (req, res) {
+        var matchedGame;
+        var current;
+        var secondCurrentOption;
+        var secondOption;
+
+        for (var x in games) {
+            var matches = 0;
+            var closeMatches = 0;
+            for (var i = 0; i < req.body.scores.length; i++) {
+                if (req.body.scores[i] === games[x].scores[i]) {
+                    matches++;
+                } else if (Math.abs(req.body.scores[i] - games[x].scores[i]) === 1) {
+                    closeMatches++;
+                }
+            }
+
+            if (isNaN(current) == false) {
+                if (matches > current) {
+                    current = matches;
+                    if (current !== 0) {
+                        matchedGame = games[x];
+                    }
+                };
+            } else {
+                current = matches;
+                if (current !== 0) {
+                    matchedGame = games[x];
+                }
+            }
+            if (isNaN(secondCurrentOption) == false) {
+                if (closeMatches > secondCurrentOption) {
+                    secondCurrentOption = closeMatches;
+                    if (closeMatches !== 0) {
+                        secondOption = games[x];
+                    }
+                };
+            } else {
+                secondCurrentOption = closeMatches;
+                if (closeMatches !== 0) {
+                    secondOption = games[x];
+                }
+            }
+        }
+
+        if (matchedGame) {
+            res.json(matchedGame);
+        } else if (secondOption) {
+            res.json(secondOption);
+        } else {
+            res.json({ name: 'No game found!', photo: '' });
+        }
+    });
+
+    app.get('/api/gameadd', function (req, res) {
+        res.json(newGames);
+    });
+
+    app.post('/api/gameadd', function (req, res) {
+        var newgame = req.body;
+        games.push(newgame);
+        newGames.push(newgame);
+
+        res.json(newgame);
+    });
+};
